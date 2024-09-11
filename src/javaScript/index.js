82057a516cff580475421b57b98d85adca260f28
Jihyeon1213 <>
2024-09-11 22:45:35 +0900

    Add bar graph color changes

--- src/javaScript/index.js
+++ src/javaScript/index.js
@@ -17,21 +17,46 @@ function merge(left, right) {
 	return [...sortedArr, ...left, ...right];
 }
 
-function mergeSort(arr) {
-	if (arr.length === 1) return arr;
+
+async function mergeSort(arr, startIndex) {
+	if (arr.length === 1) {
+		return arr;
+	}
+
 	const boundary = Math.ceil(arr.length / 2);
+
 	const left = arr.slice(0, boundary);
+	for (let index = 0; index < left.length; index++) {
+		const leftEl = document.getElementById(`graph-${index + startIndex}`);
+		leftEl.getElementsByTagName("div")[0].style.backgroundColor = "red";
+	}
+	
 	const right = arr.slice(boundary);
-	return merge(mergeSort(left), mergeSort(right));
+	for (let index = boundary; index < arr.length; index++) {
+		const rightEl = document.getElementById(`graph-${index + startIndex}`);
+		rightEl.getElementsByTagName("div")[0].style.backgroundColor = "yellow";
+	}
+
+	const leftSorted = await new Promise((resolve) => {
+		setTimeout(() => resolve(left), 1000);
+	})
+		.then(result => mergeSort(result, startIndex));
+
+	const rightSorted = await new Promise((resolve) => {
+		setTimeout(() => resolve(right), 1000);
+	})
+		.then(result => mergeSort(result, startIndex + boundary));
+
+	return merge(leftSorted, rightSorted);
 }
 
-function clickSumbit () {
+function clickSumbit() {
 	let filteredText = '';
 
 	for (let index = 0; index < inputNumber.value.length; index++) {
 		let char = inputNumber.value[index];
 
-		if((char >= '0' && char <= '9') || char === ','){
+		if ((char >= '0' && char <= '9') || char === ',') {
 			filteredText += char;
 		}
 	}
@@ -46,28 +71,26 @@ function clickSumbit () {
 	createGraphBar();
 }
 
-function createGraphBar () {
-	const sortedArray = mergeSort(inputNumberArray);
-	console.log(sortedArray);
-	
+function createGraphBar() {
 	for (let index = 0; index < inputNumberArray.length; index++) {
 		const graphBarHeight = 100 * (inputNumberArray[index] / 30);
 		const graphBox = document.createElement("li");
 		const graphBarValue = document.createElement("p");
 		const graphBar = document.createElement("div");
+		graphBox.id = "graph-" + index;
 
 		graphBarValue.innerText = inputNumberArray[index];
 		graphBar.style.height = graphBarHeight + "px";
-		
+
 		graphBox.appendChild(graphBarValue);
 		graphBox.appendChild(graphBar);
 		graphContainer.appendChild(graphBox);
 	}
-
+	mergeSort(inputNumberArray, 0);
 	isGraph = true;
 }
 
-function resetGraphBar () {
+function resetGraphBar() {
 	graphContainer.removeChild(document.querySelectorAll(".li"));
 }
 
